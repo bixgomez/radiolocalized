b22b670c0fa3fc5035365a644e74da501a7ad51b
Richard Gilbert <gilbert67@gmail.com>
2021-03-10 10:51:12 -0800

    Getting Leaflet stuff in place...

--- web/themes/custom/radiolocalized/js/script.js
+++ web/themes/custom/radiolocalized/js/script.js
@@ -16,11 +16,44 @@
           var thisLonVal = $(thisLon).text();
           $(thisLink).mouseenter(function() {
             displayInfo('Lat/Lon: ' + thisLatVal + ', ' + thisLonVal);
+            centerMap(thisLatVal,thisLonVal)
           });
         });
       }
     }
   };
+
+  // Scoping jq vars : https://stackoverflow.com/questions/4153855/variable-scope-outside-jquery-function
+  Drupal.behaviors.simpleLeafletSetup = {
+    attach: function attach(context, settings) {
+
+      $('body', context).once('myLeafletSetup').each(function () {
+
+        displayInfo('Leaflet comin at ya');
+
+        $mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYml4Z29tZXoiLCJhIjoiY2trbWFqM2NyMGcxNDJvcnJsczJuZGtwOSJ9.BMugFXrzYKMYDJYcMfCwag', {
+          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+          maxZoom: 18,
+          id: 'mapbox/streets-v11',
+          tileSize: 512,
+          zoomOffset: -1,
+          accessToken: 'your.mapbox.access.token'
+        }).addTo($mymap);
+
+        var marker = L.marker([51.5, -0.09]).addTo($mymap);
+
+      });
+
+    }
+  };
+
+  function centerMap(lat,lon) {
+    var $info_dest = $('#js-info');
+    $info_dest.text('lat/lon:' + lat + lon);
+    var marker = L.marker([lat, lon]).addTo($mymap);
+  }
 
   function displayInfo(info) {
     var $info_dest = $('#js-info');
