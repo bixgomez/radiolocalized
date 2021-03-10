fb11229bb64a19533ca68341f556ebc1861f6439
Richard Gilbert <gilbert67@gmail.com>
2021-03-09 22:40:46 -0800

    Further js data experiments.

--- web/themes/custom/radiolocalized/js/script.js
+++ web/themes/custom/radiolocalized/js/script.js
@@ -6,30 +6,25 @@
 
   Drupal.behaviors.getCoordsFromLinks = {
     attach: function attach(context, settings) {
-
       var $song_teasers = $(context).find('.song-teaser');
-
       if ($song_teasers.length) {
-
         $.each($song_teasers, function(index, value) {
           var thisLink = $(this).find('a');
-
           var thisLat = $(this).find('li.lat');
           var thisLon = $(this).find('li.lon');
-
           var thisLatVal = $(thisLat).text();
           var thisLonVal = $(thisLon).text();
-
           $(thisLink).mouseenter(function() {
-            console.log(thisLatVal);
-            console.log(thisLonVal);
+            displayInfo('Lat/Lon: ' + thisLatVal + ', ' + thisLonVal);
           });
-
         });
-
       }
-
     }
   };
+
+  function displayInfo(info) {
+    var $info_dest = $('#js-info');
+    $info_dest.text(info);
+  }
 
 })(jQuery, Drupal);
